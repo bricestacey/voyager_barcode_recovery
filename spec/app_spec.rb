f2d887cfdc37446042fc6b2e4b434202b0cbd527
Brice Stacey <bricestacey@gmail.com>
2011-08-25 16:53:52 -0400

    Add more acceptance specs

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -10,14 +10,41 @@ describe Barcode do
   end
 
   describe "POST /", :type => :request do
-    before(:each) do
-      @patron = Factory.build(:patron)
-      Barcode::Patron.stub(:find_by_ums) {@patron}
-    end
-    it "should send an email to the patron" do
-      Pony.should_receive(:mail).once
+    context "a valid UMS number is posted" do
+      before(:each) do
+        @patron = Factory.build(:patron)
+        Barcode::Patron.stub(:find_by_ums) {@patron}
+      end
 
-      post '/', :pid => '12345678'
+      it "should send an email to the patron" do
+        Pony.should_receive(:mail).once
+
+        post '/', :pid => '12345678'
+      end
+
+      it "should set a success message" do
+        post '/', :pid => '12345678'
+
+        last_response.body.should include "An email is on the way!"
+      end
+    end
+
+    context "an invalid UMS number is posted" do
+      before(:each) do
+        Barcode::Patron.stub(:find_by_ums) {nil}
+      end
+
+      it "should not send an email" do
+        Pony.should_receive(:mail).never
+
+        post '/', :pid => '12345678'
+      end
+
+      it "should set an error message" do
+        post '/', :pid => '12345678'
+
+        last_response.body.should include "We couldn't find your UMS number in our system."
+      end
     end
   end
 end
